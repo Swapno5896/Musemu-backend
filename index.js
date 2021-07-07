a059208d1f9e2d0b52f7f225d2be5f98503dee07
swapno5896 <swapno5896@yahoo.com>
2021-07-08 04:07:12 +0600

    backend is almost done

--- index.js
+++ index.js
@@ -24,35 +24,84 @@ client.connect((err) => {
   // events
   const eventsCollection = client.db("museume").collection("events");
   // gallary
-  const eventsCollection = client.db("museume").collection("gallary");
+  const gallaryCollection = client.db("museume").collection("gallary");
 
-  //   add some data
-  app.post("/addBlog", (req, res) => {
+
+
+
+  // post multiple Events
+  app.post("/addEvent", (req, res) => {
     const fakeBlog = req.body;
-    console.log(fakeBlog);
-    blogCollection.insertOne(fakeBlog).then((result) => {
+    // console.log(fakeBlog);
+    eventsCollection.insertOne(fakeBlog).then((result) => {
       console.log(result.insertedCount);
     });
   });
 
-  // get some data
+  // post multiple Events
+  app.post("/addGallary", (req, res) => {
+    const fakeBlog = req.body;
+    // console.log(fakeBlog);
+    gallaryCollection.insertOne(fakeBlog).then((result) => {
+      console.log(result.insertedCount);
+    });
+  });
 
-  app.get("/getBlog", (req, res) => {
-    blogCollection.find({}).toArray((err, document) => {
+  //  get all Events
+  app.get("/getEvent", (req, res) => {
+    eventsCollection.find({}).toArray((err, document) => {
       res.send(document);
     });
   });
 
-  // delete
 
-  const ObjectId = require("mongodb").ObjectId;
-
-  app.delete("/deletBlog/:id", (req, res) => {
-    // console.log(req.params.id);
-    blogCollection
-      .deleteOne({ _id: ObjectId(req.params.id) })
-      .then((result) => console.log(result));
+  //  get all Events
+  app.get("/getGallary", (req, res) => {
+    gallaryCollection.find({}).toArray((err, document) => {
+      res.send(document);
+    });
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //  
+  //   add some data
+  // app.post("/addBlog", (req, res) => {
+  //   const fakeBlog = req.body;
+  //   console.log(fakeBlog);
+  //   blogCollection.insertOne(fakeBlog).then((result) => {
+  //     console.log(result.insertedCount);
+  //   });
+  // });
+
+  // get some data
+
+  // app.get("/getBlog", (req, res) => {
+  //   blogCollection.find({}).toArray((err, document) => {
+  //     res.send(document);
+  //   });
+  // });
+
+  // // delete
+
+  // const ObjectId = require("mongodb").ObjectId;
+
+  // app.delete("/deletBlog/:id", (req, res) => {
+  //   // console.log(req.params.id);
+  //   blogCollection
+  //     .deleteOne({ _id: ObjectId(req.params.id) })
+  //     .then((result) => console.log(result));
+  // });
 
   // const ObjectId = require("mongodb").ObjectId; age kora hyca tai akn kora lagbe na
 
@@ -63,33 +112,33 @@ client.connect((err) => {
   //     .then((result) => console.log(result));
   // });
 
-  app.get("/blogs/:id", (req, res) => {
-    blogCollection
-      .find({ _id: ObjectId(req.params.id) })
-      .toArray((err, document) => {
-        res.send(document);
-      });
-  });
+  // app.get("/blogs/:id", (req, res) => {
+  //   blogCollection
+  //     .find({ _id: ObjectId(req.params.id) })
+  //     .toArray((err, document) => {
+  //       res.send(document);
+  //     });
+  // });
 
-  // here we'll update our data
-  app.patch("/update", (req, res) => {
-    // console.log(req.params.id);
-    // console.log(req.params.id,req.body.status);
-    console.log("four", req.body.status, req.body.id);
-    // const ObjectId = require("mongodb").ObjectId;
-    bookedServiceCollection
-      .updateOne(
-        { _id: ObjectId(req.body.id) },
-        {
-          $set: { status: req.body.status },
-        }
-      )
+  // // here we'll update our data
+  // app.patch("/update", (req, res) => {
+  //   // console.log(req.params.id);
+  //   // console.log(req.params.id,req.body.status);
+  //   console.log("four", req.body.status, req.body.id);
+  //   // const ObjectId = require("mongodb").ObjectId;
+  //   bookedServiceCollection
+  //     .updateOne(
+  //       { _id: ObjectId(req.body.id) },
+  //       {
+  //         $set: { status: req.body.status },
+  //       }
+  //     )
 
-      .then((result) => {
-        console.log("three", result);
-        res.send(result);
-      });
-  });
+  //     .then((result) => {
+  //       console.log("three", result);
+  //       res.send(result);
+  //     });
+  // });
 });
 
 // git push heroku main
